75d5984ab0223416617fc9db5e96567efae71e76
Roshan <roshan@capitalnumbers.com>
2020-09-08 09:56:32 +0530

    removed

--- Test/js/html5.js
+++
@@ -1,1 +0,0 @@
-for(i in lst='article|aside|audio|canvas|command|datalist|embed|figcaption|figure|footer|header|hgroup|keygen|mark|meter|nav|output|progress|rp|rt|ruby|section|source|summary|time|video'.split('|'))document.createElement(lst[i]);
