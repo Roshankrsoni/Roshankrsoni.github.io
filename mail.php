0716b4ef95fba9f0ce69fcc87fcc115ccd87f2d9
Roshan Kr Soni <roshankrsoni1@gmail.com>
2021-10-23 17:26:30 +0530

    test

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "info@roshankrsoni.in";
+    $email_to = "info@roshankrsoni.me";
     $email_subject = "Query form data";
  
     function died($error) {
