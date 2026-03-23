aa36c0c9101f52728b48aeac862ab27a1de68b04
Roshan Kr <roshankrsoni1@gmail.com>
2026-03-23 18:59:26 +0530

    Updates

---
+++ assets/arrow-right-Bq4ikWbT.js
@@ -0,0 +1,6 @@
+import{c as o}from"./index-7XRZLVhV.js";/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],c=o("arrow-right",r);export{c as A};
