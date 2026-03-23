e8dc32cbf5500dde7ecdb1673dd2d5e3ce4223c2
Roshan Kr <roshankrsoni1@gmail.com>
2026-03-23 19:36:12 +0530

    Updates

--- assets/icons-Co4XQwLI.js
+++
@@ -1,6 +0,0 @@
-import{c as s,T as r,a,b as i,d as t,e as d,f as c,g as u,h as l,i as o,k as b,l as T,n as f,o as p,p as B,q as g,s as k}from"./index-BAmu3x38.js";/**
- * @license lucide-react v0.546.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],w=s("external-link",h),x=n=>{const e=n.toLowerCase();return e.includes("node")?r:e.includes("typescript")?a:e.includes("javascript")?i:e.includes("tailwind")?t:e.includes("react")?d:e.includes("next")?c:e.includes("express")?u:e.includes("motion")?l:e.includes("mysql")?o:e.includes("postgres")||e.includes("sql")||e.includes("redis")||e.includes("database")?b:e.includes("vercel")?T:e.includes("websocket")?f:e.includes("aws")?p:e.includes("mongo")?B:e.includes("design")?g:k};export{w as E,x as g};
