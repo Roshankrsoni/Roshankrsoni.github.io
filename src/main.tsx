a3041449253675429eaeb8ef724d1e77aa91150e
Roshan Kr <roshankrsoni1@gmail.com>
2026-03-31 23:15:19 +0530

    feat: integrate mixpanel analytics for session tracking and event capture

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('7c3e503458c403a0ed52158cc05aade7', {
+  autocapture: true,
+  record_sessions_percent: 100,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
